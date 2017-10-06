234095a1f4d340afde2becffb51905031db33b22
Isabel G Herrick <isabelherrick@yahoo.com>
2017-10-06 11:05:48 -0500

    https://imgur.com/a/o1ml6

--- ColoradoBluesGUI.java
+++ ColoradoBluesGUI.java
@@ -23,7 +23,7 @@ public class ColoradoBluesGUI extends javax.swing.JFrame {
     public ColoradoBluesGUI() {
         initComponents();
     }
-
+ 
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
